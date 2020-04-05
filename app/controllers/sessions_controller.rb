d75718945a285200351976c256d5b0db1cfffad3
kzok <yes.notyet@gmail.com>
2020-04-05 23:26:55 +0900

    Rails Tutorial 8.2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザログイン後にユーザ情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-  end
-
-  def destroy
   end
 end
