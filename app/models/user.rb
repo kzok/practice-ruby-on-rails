bdac103144dede2bcff8d8cdfd912326a6548199
kzok <yes.notyet@gmail.com>
2020-04-12 00:10:54 +0900

    Rails Tutorial 9.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:remember_token, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
   def authenticated?(token)
